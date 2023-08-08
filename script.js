1d8eb9f9898d535070febae9e031fa964a19b9f5
Mac Neil <33123884+macneil265@users.noreply.github.com>
2023-08-08 11:21:05 +0200

    Final

--- script.js
+++ script.js
@@ -30,15 +30,17 @@ const quizData = [
         c: "God of War",
         d: "Cradle to the grave",
         correct: "a",
-    }, {
+    }, 
+    {
         question: "What is the best sport you have participated in?",
         a: "Football",
         b: "Basket Ball",
         c: "Tennis",
         d: "Rugby",
         correct: "a",
-    }, {
-        question: "What does HTML stand for",
+    }, 
+    {
+        question: "What does HTML stand for?",
         a: "Hyper Text Language",
         b: "Hyper Text Markup Language",
         c: "God of War",
@@ -47,60 +49,59 @@ const quizData = [
     }
 ];
 
-const questionEl = document.getElementById
-("question");
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
 const submitBtn = document.getElementById("submit");
 
-
 let currentQuiz = 0;
-let answer = 0;
+let score = 0; // Initialize the score
 
 loadQuiz();
 
 function loadQuiz() {
     const currentQuizData = quizData[currentQuiz];
-
     questionEl.innerText = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
     b_text.innerText = currentQuizData.b;
     c_text.innerText = currentQuizData.c;
     d_text.innerText = currentQuizData.d;
 }
-//code for the answer
-function getSelected(){
-    const answerEls = document.querySelectorAll(".answer");
 
+function getSelected() {
     let answer = undefined;
-
-    answerEls.forEach(answerEl => {
-        if(answerEl.checked){
-          answer =  answerEl.id;
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
         }
     });
-
     return answer;
 }
 
-submitBtn.addEventListener("click", () => {
-    //Check to see the answer
-    const answer = getSelected();
-    if(answer) {
-        currentQuiz++;
-        if(currentQuiz < quizData.length){
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
 
-            loadQuiz();
-            } else {
-              //TODO: Show results
-                alert("You finished, get yourself a glass of Sobo");
-            }
-    
-    
-       
+submitBtn.addEventListener("click", () => {
+    // Check the selected answer
+    const selectedAnswer = getSelected();
+    if (selectedAnswer === quizData[currentQuiz].correct) {
+        score++;
     }
 
-    
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+        // Load the next question
+        loadQuiz();
+        deselectAnswers(); // Clear the selected answer for the new question
+    } else {
+        // Show results
+        quiz.innerHTML = `<h2>You answered correctly ${score}/${quizData.length} questions.</h2>`;
+    }
 });
